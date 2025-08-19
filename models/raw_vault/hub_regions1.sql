e0458fbaeee64b5228af868273cf48a42a58dc8a
sweta-sai1991 <swetasaibhagat@gmail.com>
2025-08-19 22:59:16 +0530

    Delete models/raw_vault directory
    deleting the models created from automate_dv

--- models/raw_vault/hub_regions1.sql
+++
@@ -1,8 +0,0 @@
-{%- set source_model = "stg_regions"   -%}
-{%- set src_pk = "REGION_PK"          -%}
-{%- set src_nk = "REGION_ID"          -%}
-{%- set src_ldts = "LOAD_DATETIME"      -%}
-{%- set src_source = "RECORD_SOURCE"    -%}
-
-{{ automate_dv.hub(src_pk=src_pk, src_nk=src_nk, src_ldts=src_ldts,
-                   src_source=src_source, source_model=source_model) }}
--- models/raw_vault/link_customer_customer_demo1.sql
+++
@@ -1,8 +0,0 @@
-{%- set source_model = "stg_customer_customer_demo"        -%}
-{%- set src_pk = "CUSTOMER_CUSTOMER_DEMO_PK"         -%}
-{%- set src_fk = ["CUSTOMER_PK", "CUSTOMER_TYPE_PK"] -%}
-{%- set src_ldts = "LOAD_DATETIME"           -%}
-{%- set src_source = "RECORD_SOURCE"         -%}
-
-{{ automate_dv.link(src_pk=src_pk, src_fk=src_fk, src_ldts=src_ldts,
-                    src_source=src_source, source_model=source_model) }}
