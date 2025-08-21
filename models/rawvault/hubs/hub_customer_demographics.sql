86c8c85768c146e0acbec336a2eafe30c9f9ee18
sweta-sai1991 <swetasaibhagat@gmail.com>
2025-08-21 10:32:09 +0530

    Delete models/rawvault directory
    deletion for demo

--- models/rawvault/hubs/hub_customer_demographics.sql
+++
@@ -1,9 +0,0 @@
-
--- Auto-generated Hub: customer_demographics
-{{ config(materialized='table') }}
-select
-   md5(cast(customer_type_id as string)) as customer_demographics_hk,
-   customer_type_id,
-   current_timestamp() as load_date,
-   'customer_demographics' as record_source
-from {{ source('bigquery', 'customer_demographics') }}
--- models/rawvault/hubs/hub_regions.sql
+++
@@ -1,9 +0,0 @@
-
--- Auto-generated Hub: regions
-{{ config(materialized='table') }}
-select
-   md5(cast(region_id as string)) as regions_hk,
-   region_id,
-   current_timestamp() as load_date,
-   'regions' as record_source
-from {{ source('bigquery', 'regions') }}
